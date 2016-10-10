b466ddab8dbecb1894720f592f13e8005765a1d4
Mahmoud Tohmaz <mtohmaz@ncsu.edu>
2016-10-09 20:29:39 -0400

    Added basic navbar on top with logo image.

---
+++ angular/app/nav-bar.component.ts
@@ -0,0 +1,75 @@
+import {Component, ViewEncapsulation, OnInit} from "@angular/core";
+//import {Routes} from "./../../config/route.config";
+@Component({
+    selector: "topnav",
+    //templateUrl: "app/main/topNavbar/TopNavbar.html",
+    template: `
+      <div class="topnav">
+        <div id="logo">
+          <img src="../logo.png" />
+        </div>
+        <ul>
+          <li><a routerLink="/list-view" routerLinkActive="active">List View</a></li>
+          <li><a routerLink="/create-event" routerLinkActive="active">Create Event</a></li>
+          <li><a routerLink="/my-events" routerLinkActive="active">My Events</a></li>
+        </ul>
+      </div>
+    `,
+    encapsulation: ViewEncapsulation.None,
+    styles: [`
+      .topnav{
+        white-space: nowrap;
+        overflow: hidden;
+      }
+      .topnav ul {
+        list-style: none;
+        background-color: #444;
+        text-align: center;
+        margin: 0;
+        padding: 0;
+      }
+      .topnav li {
+        line-height: 40px;
+        border-bottom: 1px solid #888;
+        display: inline-block;
+        front-size: 1.2em;
+        height: 40px;
+        width: 20%;
+      }
+      .topnav a {
+        text-decoration: none;
+        color: #fff;
+        display: block;
+        transition: .3s background-color;
+      }
+      .topnav a:hover {
+        background-color: #005f5f;
+      }
+      .topnav a.active {
+        bcakground-color: #fff;
+        color: #333;
+        cursor: default;
+      }
+      #logo img{
+        height: 40px;
+        width: 5%;
+        float:left
+      }
+    `],
+})
+
+/*export class TopNavbarComponent  {
+    showNavBar: boolean = false;
+
+
+    constructor(private globalEventsManager: GlobalEventsManager) {
+        this.globalEventsManager.showNavBar.subscribe((mode)=>{
+            this.showNavBar = mode;
+        });
+
+    }
+
+
+}*/
+
+export class NavBarComponent { }
